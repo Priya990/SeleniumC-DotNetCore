1d8c670be3b16b1f0587540d07bf615ba625cf14
Priya R <Priya.R@marlabs.com>
2021-03-02 12:20:22 +0530

    Initial Commit

---
+++ SeleniumTestNetCore/CustomControls.cs
@@ -0,0 +1,20 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SeleniumTestNetCore
+{
+    public class CustomControls : DriverHelper
+    {
+        public static void ComboBox(String controlName, String Value)
+        {
+            IWebElement comboboxControl = Driver.FindElement(By.XPath($"//input[@id='{controlName}-awed']"));
+            comboboxControl.Clear();
+            comboboxControl.SendKeys(Value);
+
+            Driver.FindElement(By.XPath($"//div[@id='{controlName}-dropmenu']//li[text()='{Value}']")).Click();
+
+        }
+    }
+}
